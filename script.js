9bc790e9d5cdc0a8cd674165d3a7431e4d084895
epicraft1234 <91766298+epicraft1234@users.noreply.github.com>
2025-02-13 05:45:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let clickCount = 0;
+let klik = 0;
+
+    function myFunction(){
+            
+        const element = document.getElementById("b-quest")  || document.getElementById("b-quest2") || document.getElementById("b-quest2-5")  || document.getElementById("b-quest3")|| document.getElementById("b-quest4");
+        const huruf = document.getElementById("a-quest") || document.getElementById("a-quest2") || document.getElementById("a-quest2-5") ||  document.getElementById("a-quest3")||  document.getElementById("a-quest4");
+        const liQuest = document.getElementById("li-questb") || document.getElementById("li-questb2");
+        const kata = document.getElementById("kata");
+        
+        
+        if (clickCount == 0) {
+            element.classList.add("b-quest2-5");
+            element.id = "b-quest2-5";
+            huruf.classList.add("a-quest2-5");
+            huruf.id = "a-quest2-5";
+            kata.innerText = "ええ、 なんで？？:(";
+            
+            
+
+        }
+         
+        else if (clickCount == 1) {
+            element.classList.add("b-quest2");
+            element.id = "b-quest2";
+            huruf.classList.add("a-quest2");
+            huruf.id = "a-quest2";
+            kata.textContent = "ええ、 なんで？？:(";
+            
+        } 
+        else if (clickCount == 2) {
+            element.classList.add("b-quest3");
+            element.id = "b-quest3";
+            huruf.classList.add("a-quest3");
+            huruf.id = "a-quest3";
+            
+        } else if (clickCount == 3) {
+            element.classList.add("b-quest4");
+            element.id = "b-quest4";
+            huruf.classList.add("a-quest4");
+            huruf.id = "a-quest4";
+        } else if (clickCount == 4) {
+            element.classList.add("b-quest5");
+            element.id = "b-quest5";
+            huruf.classList.add("a-quest5");
+            huruf.id = "a-quest5";
+            liQuest.classList.add("li-questb2");
+            liQuest.id = "li-questb2";
+        }
+            
+        clickCount++;
+
+    }
+    
+    
+        
