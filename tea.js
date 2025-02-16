f088336d9b360833c5332b2a963aaade31bbe5b8
Haru <91766298+epicraft1234@users.noreply.github.com>
2025-02-16 20:51:33 +0700

    Update tea.js

--- tea.js
+++ tea.js
@@ -1,1 +1,14 @@
+var stage = new createjs.Stage("myCanvas");
+      var w = stage.canvas.width;
+      var h = stage.canvas.height;
 
+      var text = new createjs.Text(
+          "the longer I'm with you\nthe more I love you", 
+          "bold 24px Arial", "#fff"
+      );
+      text.textAlign = "center";
+      text.x = w / 2;
+      text.y = h / 2 - text.getMeasuredLineHeight() / 2;
+
+      stage.addChild(text);
+      stage.update();
